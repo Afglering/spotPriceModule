6655a9bbff749ae4ce6c716e31cda09b0c6c39b7
AlexGlering <afglering@gmail.com>
2023-10-06 19:29:15 +0100

    Minor adjustments + added PLC connection prompt with retry, continue and exit functionality

--- excel_module.py
+++ excel_module.py
@@ -9,5 +9,6 @@ def export_to_excel(prices_df, percentiles_df):
         with pd.ExcelWriter('SpotPrices.xlsx') as writer:
             prices_df.to_excel(writer, sheet_name='Prices', index=False)
             percentiles_df.to_excel(writer, sheet_name='Percentiles', index=False)
+            logging.info("Successfully exported dataframes to Excel.")
     except Exception as e:
         logging.error(f"Error while exporting dataframes to Excel: {e}")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from config_module import *
 from api_module import *
+from logging_module import setup_logging
 from plc_module import *
 from data_processing_module import *
 from excel_module import *
@@ -13,6 +14,12 @@ y_min_percentile = None
 price_diff_eur = None
 avg_price_eur = None
 current_hour_price_avg_EUR = None
+plc_connected = False
+data_to_write = None
+client = None
+
+# Setup logging
+setup_logging()
 
 # Print the logo
 print_logo()
@@ -25,15 +32,32 @@ if current_id not in AUTHORIZED_IDS:
     exit(1)
 logging.info(f"Authorized access by MAC address: {current_id}")
 
-# Initialize Modbus RTU client
-client = setup_plc_client(SERIAL_PORT, BAUD_RATE, PARITY, STOP_BITS, BYTE_SIZE)
+# Ask the user if they want to connect to the PLC
+connect_to_plc = input("Do you want to connect to the PLC? (y/n): ").strip().lower()
+while True:
+    if connect_to_plc == 'y':
+        # Initialize Modbus RTU client
+        client = setup_plc_client(SERIAL_PORT, BAUD_RATE, PARITY, STOP_BITS, BYTE_SIZE)
 
-# Connect to the PLC
-if client.connect():
-    logging.info("Successfully connected to the PLC.")
-else:
-    logging.error("Failed to connect to the PLC.")
-    exit(1)
+        # Connect to the PLC
+        if client.connect():
+            plc_connected = True
+            logging.info("Successfully connected to the PLC.")
+            break  # Exit the loop if the connection is successful
+        else:
+            logging.error("Failed to connect to the PLC.")
+            choice = input("Do you want to (r)etry, (m)ove on, or (e)xit? ").strip().lower()
+
+            if choice == 'r':
+                continue  # Retry the connection
+            elif choice == 'm':
+                break  # Move on without connecting
+            elif choice == 'e':
+                exit(1)  # Exit the program
+            else:
+                print("Invalid choice. Please select 'r', 'm', or 'e'.")
+    else:
+        break  # Exit the loop if the user doesn't want to connect to the PLC
 
 # Fetch the exchange rate if the API key is valid
 if validate_exchange_rate_api_key(api_key, exchange_rate_api_url):
@@ -52,7 +76,7 @@ if data:
     # Get the current hour and display the price for DK1, DK2, and the average of the two prices
     current_hour_price_DK1_EUR, current_hour_price_DK2_EUR, current_hour_price_avg_EUR = get_current_hour_prices(
         prices_df)
-    print("*** CURRENT HOUR PRICE POINT ***")
+    print("\n*** CURRENT HOUR PRICE POINT ***")
     print(
         f'The price for the current hour ({dt.now().hour}) is {current_hour_price_DK1_EUR} EUR/MWh for DK1 and '
         f'{current_hour_price_DK2_EUR} EUR/MWh for DK2. \n'
@@ -73,8 +97,8 @@ if data:
 
     # Calculate percentiles
     x = input(f"Enter the xth maximum percentile (e.g., 0.66 for 66%), or press ENTER to use last value ({x_last}): ")
-    y = input(f"Enter the yth minimum percentile (e.g., 0.33 for 33%), or press ENTER to use last value ({y_last}): \n")
-    percentiles_df = calculate_percentiles(prices_df, x, y)
+    y = input(f"Enter the yth minimum percentile (e.g., 0.33 for 33%), or press ENTER to use last value ({y_last}):\n")
+
 
     # Use last values if user presses ENTER
     if not x:
@@ -82,28 +106,33 @@ if data:
     if not y:
         y = y_last
 
+    percentiles_df = calculate_percentiles(prices_df, x, y)
+
     # Ask user if they want to sort the prices
     sort_prices(prices_df)
 
     # Save the specified percentiles to a cache file
     save_percentiles_to_cache(x, y)
 
-    # Write data to PLC registers 40001-40007
-    data_to_write = {
-        40001: price_diff_eur,
-        40002: avg_price_eur,
-        40003: current_hour_price_DK1_EUR,
-        40004: current_hour_price_DK2_EUR,
-        40005: current_hour_price_avg_EUR,
-        40006: y_min_percentile,
-        40007: x_max_percentile
-    }
+    # Write data to PLC registers 40001-40007 if the PLC is connected
+    if plc_connected:
+        data_to_write = {
+            40001: price_diff_eur,
+            40002: avg_price_eur,
+            40003: current_hour_price_DK1_EUR,
+            40004: current_hour_price_DK2_EUR,
+            40005: current_hour_price_avg_EUR,
+            40006: y_min_percentile,
+            40007: x_max_percentile
+        }
 
-    for address, value in data_to_write.items():
-        write_data_to_plc(client, address, value, scaling_factor, slave_id)
+        for address, value in data_to_write.items():
+            write_data_to_plc(client, address, value, scaling_factor, slave_id)
 
-    # Close the connection to the PLC
-    client.close()
+    if plc_connected and client is not None:
+        # Close the connection to the PLC if it is connected and the client is not None
+        client.close()
+        logging.info("Successfully closed connection to the PLC.")
 
     # Export dataframes to Excel
     export_to_excel(prices_df, percentiles_df)
